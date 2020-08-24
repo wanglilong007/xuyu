2afb4cd8f3ddb4e830d44e3acf6ca9427faca7af
wanglilong007 <wanglilong007@gmail.com>
2020-08-24 17:08:51 +0800

    6 files modified

--- src/app/mock_books.ts
+++ src/app/mock_books.ts
@@ -1,6 +1,8 @@
 import { Book } from './book/book';
 
 export const BOOKS: Book[] = [
-  {id: "1", name: "book1"},
-  {id: "2", name: "book2"}
+  {id: "1", name: "计算机网络 自定向下学习方法"},
+  {id: "2", name: "深入理解linux内核"},
+  {id: "2", name: "面向对象分析与设计"},
+  {id: "2", name: "领域驱动设计方法"}
 ]
