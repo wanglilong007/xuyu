65725f828004029d964b7c45f5936387fb0376c6
wanglilong007 <wanglilong007@gmail.com>
2020-08-25 16:40:01 +0800

    1 file modified

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { BOOKS } from './mock_books';
 import { Book } from './book/book';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
 
-  constructor() { }
+  private url = '/api/books';
+  constructor(private http: HttpClient) { }
 
   getBooks(): Observable<Book[]> {
-    return of(BOOKS);
+    return this.http.get<Book[]>(this.url)
+    //return of(BOOKS);
   }
 }
